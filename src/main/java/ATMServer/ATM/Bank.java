2fc64b6832dcdaeb9159530a012aaf133f38d276
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2015-12-09 19:11:18 +0100

    Bank account logic

--- src/main/java/ATMServer/ATM/Bank.java
+++ src/main/java/ATMServer/ATM/Bank.java
@@ -1,16 +1,19 @@
 package ATMServer.ATM;
 
+import ATMServer.data.Account;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Bank
 {
-	Map<Long, Byte> customers = new HashMap<>();
+	Map<Long, Account> customers = new HashMap<>();
 	Map<Byte, Long> sessions = new HashMap<>();
 
 	public Bank()
 	{
 		// Create some dummy customers
-
+		customers.put(1111222233334444L, new Account(500));
+		customers.put(9999999999999999L, new Account(7000));
 	}
 }
--- src/main/java/ATMServer/data/Account.java
+++ src/main/java/ATMServer/data/Account.java
@@ -1,22 +1,65 @@
 package ATMServer.data;
 
+import java.util.LinkedList;
+
+/**
+ * Represents an Account in the Bank
+ */
 public class Account
 {
-	private long creditCard;
-	private byte securityCode;
+	private LinkedList<Integer> securityCode;
 	private int balance;
 
-	public Account(long creditCard, byte securityCode, int balance)
+	public Account(int balance)
 	{
-		this.creditCard = creditCard;
-		this.securityCode = securityCode;
+		this.securityCode = new LinkedList<>();
 		this.balance = balance;
+
+		securityCode.add(1);
+		securityCode.add(3);
+		securityCode.add(5);
+		securityCode.add(7);
 	}
 
-	public boolean validateCode(int creditCard, byte securityCode)
+	/**
+	 * Checks whether the supplied security code is next in the list.
+	 * If it is, the code is popped so the next code becomes the valid one.
+	 *
+	 * @param securityCode  Security code to try
+	 */
+	public boolean validateCode(byte securityCode)
 	{
-		return (this.creditCard == creditCard && this.securityCode == securityCode);
+		if (this.securityCode.peek() == securityCode)
+		{
+			this.securityCode.pop();
+			return true;
+		}
+		else return false;
 	}
 
+	/**
+	 * Makes a withdrawal from the user's account. Returns -1 if the withdrawal fails.
+	 *
+	 * @param amount        Amount to withdraw
+	 * @return              -1 if the user has insufficient funds, new balance otherwise
+	 */
+	public int withdraw(int amount) {
+		if (balance - amount >= 0)
+		{
+			balance = balance - amount;
+			return balance;
+		}
+		else return -1;
+	}
 
+	/**
+	 * Deposit the supplied amount.
+	 *
+	 * @param amount        Amount to deposit
+	 * @return              New account balance
+	 */
+	public int deposit(int amount) {
+		balance += amount;
+		return balance;
+	}
 }
