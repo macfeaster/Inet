89f5c2e7b85d149e1549fbd14e120526a84b7a3c
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2015-12-10 13:48:33 +0100

    Abstracted writing of instructions to socket

---
+++ src/main/java/ATMClient/util/Writer.java
@@ -0,0 +1,17 @@
+package ATMClient.util;
+
+import ATMServer.data.Instruction;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Writer
+{
+	public static void write(Instruction instruction, OutputStream out) throws IOException
+	{
+		out.write(instruction.getCommand());
+		out.write(instruction.getRawData());
+		out.write(instruction.getCode());
+		out.write(instruction.getIdentifier());
+	}
+}
