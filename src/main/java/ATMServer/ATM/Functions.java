72ba18464f51575dc81d391db0b8d551216abfea
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2015-12-10 12:40:44 +0100

    Added basic functionality

--- src/main/java/ATMServer/ATM/Functions.java
+++ src/main/java/ATMServer/ATM/Functions.java
@@ -16,14 +16,49 @@ public class Functions
 	{
 		Map<Byte, Function<Instruction, Instruction>> functions = new HashMap<>();
 
+		/**
+		 * Handle incoming instructions with command ID #1 (login)
+		 */
 		functions.put((byte) 1, instr -> {
 			Account acc = bank.getCustomer(instr.getData());
 
 			if (acc.validateCode(instr.getCode())) {
 				// Open session and return success Instruction
 				bank.getSessions().put(instr.getIdentifier(), instr.getData());
+
+				// Send a valid session response code (100 welcome)
 				return new Instruction((byte) 100);
-			} else return new Instruction((byte) 82);
+			} else
+				// If validation failed, send an invalid security response (82 invalid cc)
+				return new Instruction((byte) 82);
+		});
+
+		/**
+		 * Handle #2 (logout)
+		 */
+		functions.put((byte) 2, instruction -> {
+			// If there is a session open to log out
+			if (bank.getSessions().containsKey(instruction.getIdentifier())) {
+				// End the session
+				bank.getSessions().remove(instruction.getIdentifier());
+
+				// Return
+				return new Instruction((byte) 100);
+			} else
+				// If the session does not exist, send error response (80 invalid cmd)
+				return new Instruction((byte) 80);
+		});
+
+		/**
+		 * Handle #3 (balance)
+		 */
+		functions.put((byte) 3, instruction -> {
+			Account acc = bank.getLoggedInAccount(instruction.getIdentifier());
+
+			if (acc == null)
+				return new Instruction((byte) 80);
+
+			return new Instruction((byte) 101, acc.getBalance());
 		});
 
 		return functions;
